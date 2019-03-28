d5e306fc04a9fe595f042c838f863a2926a2157e
Ruben Salas <ruben.salasalcornada@telefonica.com>
2019-03-28 10:00:28 +0100

    Make y mandatory on dataset split

--- src/lib/utils/ml_utils.py
+++ src/lib/utils/ml_utils.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 
 
-def train_val_test_split(X, y=None, train_size=0.6, val_size=0.2, test_size=0.2, seed=None):
+def train_val_test_split(X, y, train_size=0.6, val_size=0.2, test_size=0.2, seed=None):
     len_data = len(X)
 
     train_num_samples = int(len_data * train_size)
@@ -23,16 +23,12 @@ def train_val_test_split(X, y=None, train_size=0.6, val_size=0.2, test_size=0.2,
 
     if isinstance(X, (pd.Series, pd.DataFrame)):
         X = X.iloc
+    if isinstance(y, (pd.Series, pd.DataFrame)):
+        y = y.iloc
 
     X_train = X[shuffled_indices[:train_samples_index]]
     X_val = X[shuffled_indices[train_samples_index:val_samples_index]]
     X_test = X[shuffled_indices[val_samples_index:test_samples_index]]
-
-    if y is None:
-        return (X_train, X_val, X_test), (None, None, None)
-
-    if isinstance(X, (pd.Series, pd.DataFrame)):
-        y = y.iloc
 
     y_train = y[shuffled_indices[:train_samples_index]]
     y_val = y[shuffled_indices[train_samples_index:val_samples_index]]
