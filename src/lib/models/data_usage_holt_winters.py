357a63465d12493a9c865c6d103ea598c810e075
AntonioFernandezViz <antonio.fernandezviz@telefonica.com>
2019-04-03 17:10:22 +0200

    feat/final adaptation of data usage models as experiments

--- src/lib/models/data_usage_holt_winters.py
+++ src/lib/models/data_usage_holt_winters.py
@@ -8,7 +8,6 @@ import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 
 from .model import Model
-from .. import trackers
 from ..utils.temporal_series_utils import estimated_window, initial_trend, initial_seasonal_components, \
     rmse_holt_winters
 from ..utils.temporal_series_utils import mad_mean_error, nrmsd_error_norm, residual_analysis, rmse_train
@@ -17,28 +16,24 @@ from ..utils.temporal_series_utils import mad_mean_error, nrmsd_error_norm, resi
 class DataUsageHoltWinters(Model):
 
     def __init__(self, input_shape, **hyperparameters):
-        super().__init__(input_shape)
+        super().__init__(input_shape, features_serie=False)
 
         self.min_historic = hyperparameters.get("min_historic", 5)
         self.flag_optimize_hiperparams = hyperparameters.get("flag_optimize_hiperparams", True)
-        self.n_preds = hyperparameters.get("n_preds", 3)
+        self.n_preds = hyperparameters.get("n_preds", 17)
         self.slen = hyperparameters.get("slen", 1)
         self.alpha = hyperparameters.get("alpha", 0.716)
         self.beta = hyperparameters.get("beta", 0.029)
         self.gamma = hyperparameters.get("gamma", 0.993)
 
     def fit(self, X, y, batch_size=32, epochs=1, callbacks=None, val_data=None, verbose=1):
-        history = trackers.History()
-        history.log_metrics({})
-        return history.metrics
+        return {}
 
-    def predict(self, X, batch_size=32, verbose=0):
+    def predict(self, series, batch_size=32, verbose=0):
         """
         Predict the next values of a series by applying the Triple Exponential Smoothing (a.k.a. Holt-Winters)
 
         """
-        series = list(X)
-
         if self.flag_optimize_hiperparams:
             slen = estimated_window(series, 2)
 
@@ -54,8 +49,6 @@ class DataUsageHoltWinters(Model):
 
                 self.alpha, self.beta, self.gamma = parameters[0]
                 self.slen = slen
-
-        cycle_days_past = self.input_shape[0]
 
         # Holt Winters model
         result = []
@@ -86,8 +79,7 @@ class DataUsageHoltWinters(Model):
                                             (1 - self.gamma) * seasonals[i % self.slen])
                 result.append(smooth + trend + seasonals[i % self.slen])
 
-        cumsum = np.cumsum(series[-cycle_days_past:] + result[-self.n_preds:]).tolist()
-        future_predictions = cumsum[-self.n_preds:]
+        future_predictions = result[-self.n_preds:]
 
         return future_predictions
 
@@ -101,27 +93,17 @@ class DataUsageHoltWinters(Model):
         emc_error: (float) quadratic mean error for execution date
 
         """
-        y_pred = self.predict(X, batch_size=batch_size, verbose=verbose)
+        y[1:] -= y[:-1].copy()
+        y = y.flatten().tolist()
 
-        y = list(y)
+        y_pred = self.predict(y[:-self.n_preds], batch_size=batch_size, verbose=verbose)
 
-        if (y[-self.n_preds:] and y_pred[-self.n_preds:] and len(y[-self.n_preds:]) == len(y_pred[-self.n_preds:])
-                and len(y_pred[-self.n_preds:]) > 0):
-            mean_error = mad_mean_error(y[-self.n_preds:], y_pred[-self.n_preds:])
-            normal_error = nrmsd_error_norm(y[-self.n_preds:], y_pred[-self.n_preds:])
-            residual_error = residual_analysis(y[-self.n_preds:], y_pred[-self.n_preds:])
-            emc_error = rmse_train(y[-self.n_preds:], y_pred[-self.n_preds:])
-
-        else:
-            mean_error = 0.0
-            normal_error = 0.0
-            residual_error = 1.0
-            emc_error = 0.0
         return {
-            "mean_error": mean_error,
-            "normal_error": normal_error,
-            "residual_error": residual_error,
-            "emc_error": emc_error}
+            "mean_error": mad_mean_error(y[-self.n_preds:], y_pred[-self.n_preds:]),
+            "normal_error": nrmsd_error_norm(y[-self.n_preds:], y_pred[-self.n_preds:]),
+            "residual_error": residual_analysis(y[-self.n_preds:], y_pred[-self.n_preds:]),
+            "emc_error": rmse_train(y[-self.n_preds:], y_pred[-self.n_preds:])
+        }
 
     def restore(self, file_path):
         pass
