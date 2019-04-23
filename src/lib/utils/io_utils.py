d6b35488a0f0227745a17b11580760a2373b6ba7
Ruben Salas <ruben.salasalcornada@telefonica.com>
2019-04-23 17:34:48 +0200

    Fix numpy types saving JSON

--- src/lib/utils/io_utils.py
+++ src/lib/utils/io_utils.py
@@ -5,6 +5,7 @@
 #
 
 import json
+import numpy as np
 
 
 def read_from_text(file_path):
@@ -17,6 +18,15 @@ def read_from_json(file_path):
         return json.load(f)
 
 
+def _json_default(o):
+    # Hack to save numpy number with JSON module
+    if isinstance(o, np.int32) or isinstance(o, np.int64):
+        return int(o)
+    if isinstance(o, np.float32) or isinstance(o, np.float64):
+        return float(o)
+    raise TypeError
+
+
 def save_to_json(path, obj, indent=4):
     with open(path, "w") as outfile:
-        json.dump(obj, outfile, indent=indent)
+        json.dump(obj, outfile, indent=indent, default=_json_default)
