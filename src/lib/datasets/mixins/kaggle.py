8b819bfe3c7827a9855c0e14f27ee66ec2e52d8a
Ruben Salas <ruben.salasalcornada@telefonica.com>
2019-04-29 14:27:52 +0200

    Add Kaggle credentials documentation

--- src/lib/datasets/mixins/kaggle.py
+++ src/lib/datasets/mixins/kaggle.py
@@ -35,6 +35,34 @@ class KaggleMixin:
         ----------
         download_path: str
             Path to download kaggle dataset.
+
+        Note
+        ----
+        You need to provide Kaggle credentials to download a dataset from Kaggle.
+        Sign up for a Kaggle account at https://www.kaggle.com. Then go to the 'Account' tab of
+        your user profile (``https://www.kaggle.com/<username>/account``) and select 'Create API Token'.
+        This will trigger the download of ``kaggle.json``, a file containing your API credentials.
+        Place this file in the location ``~/.kaggle/kaggle.json`` (on Windows in the location
+        ``C:\\Users\\<Windows-username>\\.kaggle\\kaggle.json`` - you can check the exact location, sans drive,
+        with ``echo %HOMEPATH%``).
+        You can define a shell environment variable ``KAGGLE_CONFIG_DIR`` to change this location to
+        ``$KAGGLE_CONFIG_DIR/kaggle.json`` (on Windows it will be ``%KAGGLE_CONFIG_DIR%\\kaggle.json``).
+        For your security, ensure that other users of your computer do not have read access to your credentials.
+        On Unix-based systems you can do this with the following command:
+
+        .. code-block:: sh
+
+            chmod 600 ~/.kaggle/kaggle.json
+
+        You can also choose to export your Kaggle username and token to the environment:
+
+        .. code-block:: sh
+
+            export KAGGLE_USERNAME=datadinosaur
+            export KAGGLE_KEY=xxxxxxxxxxxxxx
+
+        In addition, you can export any other configuration value that normally would be in the
+        ``$HOME/.kaggle/kaggle.json`` in the format ``KAGGLE_`` (note uppercase).
         """
 
         if self.kaggle_dataset_name is None:
