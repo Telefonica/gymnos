0f261166590394a2cd083b5380fd815d0f87a1e1
Ruben Salas <ruben.salasalcornada@telefonica.com>
2019-04-29 09:49:22 +0200

    Add missing mock imports for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 
-autodoc_mock_imports = ["keras", "sklearn", "joblib", "scipy", "pandas", "tqdm", "statsmodels", "cv2"]
+autodoc_mock_imports = ["keras", "sklearn", "joblib", "scipy", "pandas", "tqdm", "statsmodels", "cv2",
+                        "spacy", "comet_ml"]
 
 
 # -- Project information -----------------------------------------------------
