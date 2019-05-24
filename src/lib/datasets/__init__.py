2609dfc4f6352b1649804c6584705c15a089fcb3
Rubén Salas <ruben.salasalcornada@telefonica.com>
2019-05-25 01:30:49 +0200

    Remove cifar-10 and fashion_mnist dataset

--- src/lib/datasets/__init__.py
+++ src/lib/datasets/__init__.py
@@ -1,6 +1,4 @@
 from .boston_housing import BostonHousing
-from .cifar10 import CIFAR10
-from .fashion_mnist import FashionMNIST
 from .imdb import IMDB
 from .kddcup99 import KDDCup99
 from .mnist import MNIST
