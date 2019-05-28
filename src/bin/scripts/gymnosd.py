b13ad7397ba2e82b5cc2c0ee9db18f53362faaea
AntonioFernandezViz <antonio.fernandezviz@telefonica.com>
2019-05-28 12:46:44 +0200

    feat/refactor module and fix issues detected

--- src/bin/scripts/gymnosd.py
+++ src/bin/scripts/gymnosd.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 
-import os
-import copy
-import shutil
-import logging
 import argparse
-
+import copy
+import logging
+import os
+import shutil
 from tempfile import TemporaryDirectory
 
-from lib.logger import get_logger
-from lib.trainer import Trainer
-from lib.predictor import Predictor
-from lib.core.model import Model
 from lib.core.dataset import Dataset
-from lib.core.training import Training
-from lib.core.tracking import Tracking
 from lib.core.experiment import Experiment
+from lib.core.model import Model
+from lib.core.tracking import Tracking
+from lib.core.training import Training
+from lib.logger import get_logger
+from lib.predictor import Predictor
+from lib.trainer import Trainer
 from lib.utils.io_utils import save_to_json, read_from_json
 
 CACHE_CONFIG_PATH = os.path.join("config", "cache.json")
@@ -25,6 +24,7 @@ REGRESSION_TESTS_DIR = os.path.join("experiments", "tests")
 
 TRAINING_LOG_FILENAME = "execution.log"
 TRAINING_CONFIG_FILENAME = "training_config.json"
+ARTIFACTS_PATH = "artifacts"
 
 
 def run_experiment(training_config_path, output_path="trainings"):
@@ -63,27 +63,29 @@ def run_experiment(training_config_path, output_path="trainings"):
                                                                                          TRAINING_LOG_FILENAME))
 
 
-def run_prediction(scoring_table_path, trained_model_config_path):
-    prediction_config = read_from_json(os.path.join(trained_model_config_path, "training_config.json"))
+def run_prediction(execution_dir, values, loggging_config_path_prediction):
+    prediction_config = read_from_json(os.path.join(execution_dir, "training_config.json"))
 
-    cache_config = read_from_json(CACHE_CONFIG_PATH)
-    logging_config = read_from_json(LOGGING_CONFIG_PATH)
+    logging_config = read_from_json(loggging_config_path_prediction)
     logging.config.dictConfig(logging_config)
     logger = get_logger(prefix="Main")
 
-    prediction = Predictor(scoring_table_path=scoring_table_path,
-                           trained_model_config_path=trained_model_config_path,
-                           cache_datasets_path=cache_config["datasets"])
+    model = Model(**prediction_config["model"])
+    dataset = Dataset(**prediction_config["dataset"])
+
+    artifacts_path = os.path.join(execution_dir, ARTIFACTS_PATH)
+    model.model.restore(artifacts_path)
+    dataset.preprocessor_pipeline.restore(os.path.join(artifacts_path, "pipeline.pkl"))
+
+    predictor = Predictor(model, dataset)
 
     try:
-        prediction.predict(
-            model=Model(**prediction_config["model"]),
-            dataset=Dataset(**prediction_config["dataset"])
-        )
+        prediction = predictor.predict(values)
 
     except Exception as e:
         logger.exception("Exception ocurred: {}".format(e))
         raise
+    return prediction
 
 
 if __name__ == "__main__":
@@ -92,11 +94,9 @@ if __name__ == "__main__":
     group.add_argument("-c", "--training_config", help="sets training training configuration file",
                        action="store")
     group.add_argument("-t", "--regression_test", help="execute regression test", action="store_true")
-    group.add_argument("-p", "--scoring_table_path_and_configuration_path",
-                       help="dataset used in the prediction and configuration path used in trained mode", nargs=2,
-                       action="store")
 
     args = parser.parse_args()
+
     if args.regression_test:
         test_config_filenames = os.listdir(REGRESSION_TESTS_DIR)
         for i, test_config_filename in enumerate(test_config_filenames):
@@ -104,10 +104,5 @@ if __name__ == "__main__":
                                                                      test_config_filename, "\033[0m"))
             with TemporaryDirectory() as temp_dir:
                 run_experiment(os.path.join(REGRESSION_TESTS_DIR, test_config_filename), temp_dir)
-    elif args.scoring_table_path_and_configuration_path:
-
-        run_prediction(args.scoring_table_path_and_configuration_path[0],
-                       args.scoring_table_path_and_configuration_path[1])
-
     else:
         run_experiment(args.training_config)
--- src/lib/predictor.py
+++ src/lib/predictor.py
@@ -3,16 +3,8 @@
 #   Predictor
 #
 #
-
-import os
-import platform
-
-import GPUtil
-import cpuinfo
-
-from .datasets import ClassificationDataset
 from .logger import get_logger
-from .utils.path import chdir
+from .utils.platform_details import platform_details
 from .utils.timing import elapsed_time
 
 
@@ -20,141 +12,47 @@ class Predictor:
     """
     Entrypoint to run prediction given a model and a dataset.
 
-
-    Parameters
-    ----------
-    trained_model_config_path: str
-         Path with the directory where we load the traned model.
-    scoring_table_path: str, optional
-        Path with the directory and file where the dataset that will be scored.
-    executions_dirname: str, optional
-        Directory name where an execution for a dataset is saved.
-    execution_format: str, optional
-        Formatting for executions, it can contain named formatting options, which will be filled with
-        the values of datetime, model_name, dataset_name and experiment_name.
-    artifacts_dirname: str, optional
-        Directory name where artifacts are saved (saved model, saved preprocessors, etc ...)
-    cache_datasets_path: str, optional
-        Directory to read and save HDF5 optimized datasets.
     """
 
-    def __init__(self, trained_model_config_path="",
-                 scoring_table_path="scorings", executions_dirname="executions",
-                 execution_format="{datetime:%H-%M-%S--%d-%m-%Y}__{model_name}",
-                 artifacts_dirname="artifacts", cache_datasets_path=None):
-        self.trained_model_config_path = trained_model_config_path
-        self.scoring_table_path = scoring_table_path
-        self.executions_dirname = executions_dirname
-        self.execution_format = execution_format
-        self.artifacts_dirname = artifacts_dirname
-        self.cache_datasets_path = cache_datasets_path
-
+    def __init__(self, model, dataset):
+        self.model = model
+        self.dataset = dataset
         self.logger = get_logger(prefix=self)
 
-    def predict(self, model, dataset):
+    def predict(self, values):
         """
         Run experiment generating outputs.
 
         Parameters
         ----------
-        model: core.model.model
-        dataset: core.dataset.Dataset
-
-        Attributes
-        ----------
+        values: dataset used for prediction
 
         """
-        execution_steps_elapsed = {}
-
-        self.logger.info("Running prediction using trained model")
-
-        # LOAD ARTIFACT PATH OF OLD EXPERIMENT
-
-        trainings_dataset_execution_artifacts_path = os.path.join(self.trained_model_config_path,
-                                                                  self.artifacts_dirname)
 
         # RETRIEVE PLATFORM DETAILS
 
-        cpu_info = cpuinfo.get_cpu_info()
-
-        gpus_info = []
-        for gpu in GPUtil.getGPUs():
-            gpus_info.append({
-                "name": gpu.name,
-                "memory": gpu.memoryTotal
-            })
-
-        platform_details = {
-            "python_version": platform.python_version(),
-            "python_compiler": platform.python_compiler(),
-            "platform": platform.platform(),
-            "system": platform.system(),
-            "node": platform.node(),
-            "architecture": platform.architecture()[0],
-            "processor": platform.processor(),
-            "cpu": {
-                "brand": cpu_info["brand"],
-                "cores": cpu_info["count"]
-            },
-            "gpu": gpus_info
-        }
+        execution_steps_elapsed = {}
 
         for name, key in zip(("Python version", "Platform"), ("python_version", "platform")):
-            self.logger.info("{}: {}".format(name, platform_details[key]))
+            self.logger.info("{}: {}".format(name, platform_details(key)))
 
-        self.logger.info("Found {} GPUs".format(len(gpus_info)))
-
-        # LOAD DATASET
-
-        self.logger.info("Loading scoring table from path: {} ...".format(self.scoring_table_path))
-
-        with elapsed_time() as elapsed:
-            load_data_arguments = {}
-            if self.cache_datasets_path is not None:
-                load_data_arguments["hdf5_cache_path"] = os.path.join(self.cache_datasets_path, dataset.name + ".h5")
-
-            if isinstance(dataset.dataset, ClassificationDataset):
-                load_data_arguments["one_hot"] = dataset.one_hot
-
-            if self.cache_datasets_path is None:
-                dataset.dataset.read(self.scoring_table_path)
-            else:
-                scoring_table, y = dataset.dataset.load_data(**load_data_arguments)
-
-        execution_steps_elapsed["load_data"] = elapsed.s
-        self.logger.debug("Loading data took {:.2f}s".format(elapsed.s))
+        self.logger.info("Found {} GPUs".format(len(platform_details("gpu"))))
 
         # APPLY PREPROCESSORS
 
-        self.logger.info("Restoring preprocessors")
+        self.logger.info("Apply preprocessors")
 
         with elapsed_time() as elapsed:
-            dataset.preprocessor_pipeline.restore(
-                os.path.join(trainings_dataset_execution_artifacts_path, "pipeline.pkl"))
-
-        execution_steps_elapsed["restore_preprocessors"] = elapsed.s
-        self.logger.debug("Restoring preprocessors to train data took {:.2f}s".format(elapsed.s))
-
-        with elapsed_time() as elapsed:
-            preprocessed_scoring_table = dataset.preprocessor_pipeline.transform(scoring_table, data_desc="X_train")
+            values = self.dataset.preprocessor_pipeline.transform(values)
 
         execution_steps_elapsed["transform_preprocessors"] = elapsed.s
         self.logger.debug("Preprocessing data took {:.2f}s".format(elapsed.s))
 
-        # RESTORE MODEL
-
-        self.logger.info("Restore model")
-        with elapsed_time() as elapsed, chdir(trainings_dataset_execution_artifacts_path):
-            model.model.restore(trainings_dataset_execution_artifacts_path)
-
-        execution_steps_elapsed["Restore model"] = elapsed.s
-        self.logger.debug("Restoring prediction took {:.2f}s".format(elapsed.s))
-
         # PREDICT
 
         self.logger.info("Apply model")
-        with elapsed_time() as elapsed, chdir(trainings_dataset_execution_artifacts_path):
-            prediction = model.model.predict(preprocessed_scoring_table)
+        with elapsed_time() as elapsed:
+            prediction = self.model.model.predict(values)
 
         execution_steps_elapsed["Apply model"] = elapsed.s
         self.logger.debug("Applying model took {:.2f}s".format(elapsed.s))
