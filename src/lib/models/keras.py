5d4c07ae1665ea899d6deff5c6c388560d8ec67f
Rubén Salas <ruben.salasalcornada@telefonica.com>
2019-06-03 12:25:07 +0200

    Simplify keras model parameters

--- src/lib/models/keras.py
+++ src/lib/models/keras.py
@@ -7,7 +7,7 @@
 import os
 
 from pydoc import locate
-from keras import models
+from keras import models, layers
 
 from .model import Model
 from .mixins import KerasMixin
@@ -37,20 +37,23 @@ class Keras(KerasMixin, Model):
     This model requires one-hot encoded labels.
     """
 
-    def __init__(self, sequential, compilation):
-        loss, optimizer, metrics = self.__build_compilation_from_config(**compilation)
-        self.model = self.__build_sequential_from_config(sequential)
+    def __init__(self, input_shape, sequential, optimizer, loss=None, metrics=None):
+
+        optimizer = self.__build_optimizer_from_config(optimizer)
+        metrics = self.__build_metrics_from_config(metrics)
+        self.model = self.__build_sequential_from_config(input_shape, sequential)
         self.model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
 
-
-    def __build_compilation_from_config(self, loss, optimizer, metrics=None):
-        metrics = metrics or []
-
+    def __build_optimizer_from_config(self, optimizer):
         if isinstance(optimizer, dict):
             optimizers_ids_to_modules = read_from_json(KERAS_OPTIMIZERS_IDS_TO_MODULES_PATH)
             OptimizerClass = locate(optimizers_ids_to_modules[optimizer.pop("type")])
             optimizer = OptimizerClass(**optimizer)
 
+        return optimizer
+
+
+    def __build_metrics_from_config(self, metrics):
         metrics_funcs = []
         metrics_ids_to_modules = read_from_json(KERAS_METRICS_IDS_TO_MODULES_PATH)
         for metric_name in metrics:
@@ -60,11 +63,13 @@ class Keras(KerasMixin, Model):
             else:
                 metrics_funcs.append(metric_name)
 
-        return loss, optimizer, metrics_funcs
+        return metrics_funcs
 
 
-    def __build_sequential_from_config(self, sequential_config):
-        sequential = models.Sequential()
+    def __build_sequential_from_config(self, input_shape, sequential_config):
+        input_layer = layers.Input(input_shape)
+
+        output_layer = input_layer
         for layer_config in sequential_config:
             layer_type = layer_config.pop("type")
             if layer_type == "application":
@@ -76,6 +81,8 @@ class Keras(KerasMixin, Model):
                 LayerClass = locate(layers_ids_to_modules[layer_type])
                 layer = LayerClass(**layer_config)
 
-            sequential.add(layer)
+            output_layer = layer(output_layer)
 
-        return sequential
+        model = models.Model(inputs=[input_layer], outputs=[output_layer])
+
+        return model
