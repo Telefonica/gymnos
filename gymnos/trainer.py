de54fff941b925efed918b5d6e75b24b2a8902ac
Rubén Salas <ruben.salasalcornada@telefonica.com>
2019-07-23 15:24:44 +0200

    Move core arguments to trainer constructor

--- gymnos/trainer.py
+++ gymnos/trainer.py
@@ -17,10 +17,12 @@ from tensorflow.keras.utils import to_categorical
 
 from .trackers import History
 from .services import DownloadManager
+from .loader import load_model, load_dataset
 from .utils.text_utils import humanize_bytes
+from .utils.archiver import extract_zip, zipdir
 from .callbacks import CallbackList, TimeHistory
+from .core import Experiment, Model, Dataset, Training, Tracking
 from .utils.data import Subset, DataLoader, get_approximate_nbytes
-
 
 logger = logging.getLogger(__name__)
 
@@ -31,33 +33,74 @@ class Trainer:
 
     Parameters
     ----------
-    dl_manager: DownloadManager
-        DownloadManager to fetch data files.
-    trackings_dir: str
-        Directory to store tracking outputs. By default, current directory
+    experiment: core.Experiment
+    model: core.Model
+    dataset: core.Dataset
+    training: core.Training
+    tracking: core.Tracking
     """
 
-    def __init__(self, dl_manager=None, trackings_dir=None):
+    def __init__(self, experiment, model, dataset, training, tracking):
+        self.experiment = experiment
+        self.model = model
+        self.dataset = dataset
+        self.training = training
+        self.tracking = tracking
+
+    @staticmethod
+    def from_spec(spec):
+        """
+        Create trainer from dictionnary specifying experiment, model, dataset, training and tracking specs.
+
+        Parameters
+        ----------
+        spec: dict
+            Dictionnary with the following keys: "experiment", "model", "dataset", "training", "tracking"
+
+        Returns
+        -------
+        trainer: Trainer
+        """
+        experiment = spec.get("experiment", {})
+        model = spec.get("model", {})
+        dataset = spec.get("dataset", {})
+        training = spec.get("training", {})
+        tracking = spec.get("tracking", {})
+        return Trainer(
+            experiment=Experiment(**experiment),
+            model=Model(**model),
+            dataset=Dataset(**dataset),
+            training=Training(**training),
+            tracking=Tracking(**tracking)
+        )
+
+    def train(self, dl_manager=None, trackings_dir=None, callbacks=None):
+        """
+        Run trainer.
+
+        Parameters
+        ----------
+        dl_manager: DownloadManager
+            DownloadManager to fetch data files.
+        trackings_dir: str
+            Directory to store tracking outputs. By default, current directory
+        callbacks: list of Callback
+            Callbacks to run during training
+
+        Returns
+        -------
+        results: dict
+            Training results with the following keys: "experiment_name", "start_datetime", "end_datetime",
+            "elapsed_times", "tags", "params", "metrics", "system_info", "trackings_dir", "download_dir",
+            "extract_dir",
+        """
+
         if dl_manager is None:
             dl_manager = DownloadManager()
         if trackings_dir is None:
             trackings_dir = os.getcwd()
 
-        self.dl_manager = dl_manager
-        self.trackings_dir = trackings_dir
-
-    def train(self, model, dataset, training, tracking, callbacks=None):
-        """
-        Run an experiment
-
-        Parameters
-        ----------
-        model: core.Model
-        dataset: core.Dataset
-        training: core.Training
-        tracking: core.Tracking
-        """
-        logger.info("Running experiment with id: {}".format(tracking.run_id))
+        logger.info("Starting experiment {}".format(self.experiment.name))
 
         if callbacks is None:
             callbacks = []
@@ -103,24 +146,24 @@ class Trainer:
 
         # START TRACKING
 
-        os.makedirs(self.trackings_dir, exist_ok=True)
+        os.makedirs(trackings_dir, exist_ok=True)
 
         history_tracker = History()
-        tracking.trackers.add(history_tracker)
+        self.tracking.trackers.add(history_tracker)
 
-        tracking.trackers.start(run_id=tracking.run_id, logdir=self.trackings_dir)
+        self.tracking.trackers.start(run_id=self.tracking.run_id, logdir=trackings_dir)
 
         # LOG PARAMETERS
 
-        if tracking.log_model_params:
-            tracking.trackers.log_params(model.parameters)
+        if self.tracking.log_model_params:
+            self.tracking.trackers.log_params(self.model.parameters)
 
-        if tracking.log_training_params:
-            tracking.trackers.log_params(training.parameters)
+        if self.tracking.log_training_params:
+            self.tracking.trackers.log_params(self.training.parameters)
 
-        tracking.trackers.log_tags(tracking.tags)
+        self.tracking.trackers.log_tags(self.tracking.tags)
 
-        for model_param_name, model_param_value in training.parameters.items():
+        for model_param_name, model_param_value in self.training.parameters.items():
             str_model_param_value = str(model_param_value)
             if isinstance(model_param_value, dict):
                 str_model_param_value = ", ".join(name + "=" + val for name, val in model_param_value.items())
@@ -143,7 +186,7 @@ class Trainer:
         callbacks.on_download_and_prepare_data_begin()
 
         logger.info("Downloading/preparing data")
-        dataset.dataset.download_and_prepare(self.dl_manager)
+        self.dataset.dataset.download_and_prepare(dl_manager)
 
         callbacks.on_download_and_prepare_data_end()
 
@@ -151,13 +194,13 @@ class Trainer:
 
         # LOG DATASET PROPERTIES
 
-        dataset_info = dataset.dataset.info()
+        dataset_info = self.dataset.dataset.info()
 
         logger.debug("Dataset Features: {}".format(dataset_info.features))
         logger.debug("Dataset Labels: {}".format(dataset_info.labels))
 
-        nbytes = get_approximate_nbytes(dataset.dataset)
-        logger.debug("Full Dataset Samples: {}".format(len(dataset.dataset)))
+        nbytes = get_approximate_nbytes(self.dataset.dataset)
+        logger.debug("Full Dataset Samples: {}".format(len(self.dataset.dataset)))
         logger.debug("Full Dataset Memory Usage (approx): {}".format(humanize_bytes(nbytes)))
 
         # SPLIT DATASET INTO TRAIN AND TEST
@@ -166,32 +209,32 @@ class Trainer:
 
         logger.info("Spliting dataset into train and test")
 
-        train_samples = dataset.samples.train
-        test_samples = dataset.samples.test
+        train_samples = self.dataset.samples.train
+        test_samples = self.dataset.samples.test
 
         if 0.0 < train_samples < 1.0:
-            train_samples = math.floor(train_samples * len(dataset.dataset))
+            train_samples = math.floor(train_samples * len(self.dataset.dataset))
         if 0.0 < test_samples < 1.0:
-            test_samples = math.floor(test_samples * len(dataset.dataset))
+            test_samples = math.floor(test_samples * len(self.dataset.dataset))
 
         logger.debug("Using {} samples for training".format(train_samples))
-        logger.debug("Using {} samples for validation".format(test_samples))
+        logger.debug("Using {} samples for testing".format(test_samples))
 
         train_indices = np.arange(train_samples)
         test_indices  = np.arange(train_samples, train_samples + test_samples)
 
-        indices = np.arange(len(dataset.dataset))
+        indices = np.arange(len(self.dataset.dataset))
 
         logger.info("Shuffling dataset")
 
-        if dataset.shuffle:
+        if self.dataset.shuffle:
             indices = np.random.permutation(indices)
 
         train_indices = indices[:train_samples]
         test_indices = indices[train_samples:(train_samples + test_samples)]
 
-        train_subset = Subset(dataset.dataset, train_indices)
-        test_subset  = Subset(dataset.dataset, test_indices)
+        train_subset = Subset(self.dataset.dataset, train_indices)
+        test_subset  = Subset(self.dataset.dataset, test_indices)
 
         callbacks.on_train_test_split_end()
 
@@ -203,14 +246,16 @@ class Trainer:
 
         logger.info("Loading data")
 
-        load_data_by_chunks = dataset.chunk_size is not None
+        load_data_by_chunks = self.dataset.chunk_size is not None
 
         if load_data_by_chunks:
-            train_loader = DataLoader(train_subset, batch_size=dataset.chunk_size, drop_last=False)
-            test_loader  = DataLoader(test_subset, batch_size=dataset.chunk_size, drop_last=False)
+            train_loader = DataLoader(train_subset, batch_size=self.dataset.chunk_size, drop_last=False)
+            test_loader  = DataLoader(test_subset, batch_size=self.dataset.chunk_size, drop_last=False)
         else:
-            train_data = DataLoader(train_subset, batch_size=len(dataset.dataset), drop_last=False, verbose=True)[0]
-            test_data = DataLoader(test_subset, batch_size=len(dataset.dataset), drop_last=False, verbose=True)[0]
+            train_data = DataLoader(train_subset, batch_size=len(self.dataset.dataset), drop_last=False,
+                                    verbose=True)[0]
+            test_data = DataLoader(test_subset, batch_size=len(self.dataset.dataset), drop_last=False,
+                                   verbose=True)[0]
 
         callbacks.on_load_data_end()
 
@@ -220,25 +265,25 @@ class Trainer:
 
         callbacks.on_fit_preprocessors_begin()
 
-        logger.info("Fitting preprocessors using training data ({})".format(dataset.preprocessors))
+        logger.info("Fitting preprocessors using training data ({})".format(self.dataset.preprocessors))
 
         if load_data_by_chunks:
-            dataset.preprocessors.fit_generator(train_loader)
+            self.dataset.preprocessors.fit_generator(train_loader)
         else:
-            dataset.preprocessors.fit(train_data[0], train_data[1])
+            self.dataset.preprocessors.fit(train_data[0], train_data[1])
 
         callbacks.on_fit_preprocessors_end()
 
         logger.debug("Fitting preprocessors took {:.2f}s".format(time_history.times["fit_preprocessors"]))
 
-        def augment_data(data):
-            if not dataset.data_augmentors:
+        def augment(data):
+            if not self.dataset.data_augmentors:
                 return data
 
             augmented_features = []
 
             for index in range(len(data[0])):
-                item = dataset.data_augmentors.transform(data[0][index])
+                item = self.dataset.data_augmentors.transform(data[0][index])
                 augmented_features.append(item)
 
             return np.array(augmented_features), data[1]
@@ -252,15 +297,15 @@ class Trainer:
             # make sure data is a list and not a tuple (can't modify tuples)
             data = list(data)
 
-            data[0] = dataset.preprocessors.transform(data[0])
+            data[0] = self.dataset.preprocessors.transform(data[0])
 
-            if dataset.one_hot:
+            if self.dataset.one_hot:
                 data[1] = to_categorical(data[1], dataset_info.labels.num_classes)
 
             return data
 
         def augment_and_preprocess(data):
-            data = augment_data(data)
+            data = augment(data)
             data = preprocess(data)
             return data
 
@@ -288,9 +333,9 @@ class Trainer:
         logger.info("Fitting model")
 
         if load_data_by_chunks:
-            train_metrics = model.model.fit_generator(train_loader, **training.parameters)
+            train_metrics = self.model.model.fit_generator(train_loader, **self.training.parameters)
         else:
-            train_metrics = model.model.fit(train_data[0], train_data[1], **training.parameters)
+            train_metrics = self.model.model.fit(train_data[0], train_data[1], **self.training.parameters)
 
         callbacks.on_fit_model_end()
 
@@ -301,8 +346,8 @@ class Trainer:
                                                                                        np.mean(metric_value),
                                                                                        np.min(metric_value),
                                                                                        np.max(metric_value)))
-        if tracking.log_model_metrics:
-            tracking.trackers.log_metrics(train_metrics)
+        if self.tracking.log_model_metrics:
+            self.tracking.trackers.log_metrics(train_metrics)
 
         # EVALUATE MODEL
 
@@ -311,9 +356,9 @@ class Trainer:
         logger.info("Evaluating model using test data")
 
         if load_data_by_chunks:
-            test_metrics = model.model.evaluate_generator(test_loader)
+            test_metrics = self.model.model.evaluate_generator(test_loader)
         else:
-            test_metrics = model.model.evaluate(test_data[0], test_data[1])
+            test_metrics = self.model.model.evaluate(test_data[0], test_data[1])
 
         callbacks.on_evaluate_model_end()
 
@@ -322,15 +367,15 @@ class Trainer:
         for metric_name, metric_value in test_metrics.items():
             logger.info("test_{}={}".format(metric_name, metric_value))
 
-        if tracking.log_model_metrics:
-            tracking.trackers.log_metrics(test_metrics, prefix="test_")
+        if self.tracking.log_model_metrics:
+            self.tracking.trackers.log_metrics(test_metrics, prefix="test_")
 
-        tracking.trackers.end()
+        self.tracking.trackers.end()
 
         callbacks.on_train_end()
 
         return OrderedDict([
-            ["run_id", tracking.run_id],
+            ["run_id", self.tracking.run_id],
             ["start_datetime", history_tracker.start_datetime.timestamp()],
             ["end_datetime", history_tracker.end_datetime.timestamp()],
             ["elapsed_times", time_history.times],
@@ -338,7 +383,7 @@ class Trainer:
             ["params", history_tracker.params],
             ["metrics", history_tracker.metrics],
             ["system_info", system_info],
-            ["trackings_dir", self.trackings_dir],
-            ["download_dir", self.dl_manager.download_dir],
-            ["extract_dir", self.dl_manager.extract_dir]
+            ["trackings_dir", trackings_dir],
+            ["download_dir", dl_manager.download_dir],
+            ["extract_dir", dl_manager.extract_dir]
         ])
