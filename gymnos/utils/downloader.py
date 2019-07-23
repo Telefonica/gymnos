bc1908d1e89da8e23f436c52b8d4d5eeb94e96d6
Rubén Salas <ruben.salasalcornada@telefonica.com>
2019-07-23 15:30:37 +0200

    Fix downloader when no content-length is available

--- gymnos/utils/downloader.py
+++ gymnos/utils/downloader.py
@@ -39,17 +39,18 @@ def download_file_from_url(url, file_path, force=False, verbose=True):
         Response object
     """
     if os.path.isfile(file_path) and not force:
-        if verbose:
-            logger.debug("File for url {} already exists in {}. Skipping".format(url, file_path))
         return
 
     response = requests.get(url, stream=True)
-    file_size = int(response.headers["Content-Length"])
     chunk_size = 1024
-    num_bars = file_size // chunk_size
 
     iterator = response.iter_content(chunk_size=chunk_size)
-    if verbose:
+
+    content_length = response.headers.get("Content-Length")
+
+    if verbose and content_length is not None:
+        file_size = int(content_length)
+        num_bars = file_size // chunk_size
         iterator = tqdm(iterator, total=num_bars, unit="KB", desc=url[:50], leave=True)
 
     with open(file_path, "wb") as fp:
