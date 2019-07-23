b5e83d7eab5bf7bf03d7cc8d14b373f8e9bc86ba
Rubén Salas <ruben.salasalcornada@telefonica.com>
2019-07-23 15:28:52 +0200

    Add missing modules init models

--- gymnos/models/__init__.py
+++ gymnos/models/__init__.py
@@ -1,6 +1,7 @@
 from .model import Model
-from .keras import Keras
+from .mte_nn import MTENN
 from .dogs_vs_cats_cnn import DogsVsCatsCNN
+from .keras import KerasClassifier, KerasRegressor
 from .data_usage_holt_winters import DataUsageHoltWinters
 from .data_usage_linear_regression import DataUsageLinearRegression
 from .unusual_data_usage_weighted_thresholds import UnusualDataUsageWT
