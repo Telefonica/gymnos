8457e61ae42db0f1a42ab4dc39b4574074ca5696
Rubén Salas <ruben.salasalcornada@telefonica.com>
2019-08-19 11:50:24 +0200

    Refactor scripts.cli module

--- scripts/cli.py
+++ scripts/cli.py
@@ -6,24 +6,81 @@
 
 import os
 import uuid
+import json
+import flask
 import argparse
+import itertools
+import numpy as np
 import logging.config
 
-
+from PIL import Image
 from collections import OrderedDict
 
-from .lib.io_utils import read_json, save_to_json
 
-from gymnos.trainer import Trainer
-from gymnos.services.download_manager import DownloadManager
+class NDArrayEncoder(flask.json.JSONEncoder):
+    """
+    Json Encoder to handle Numpy arrays
+    """
+
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
 
-def train(training_specs_json, download_dir, force_download, force_extraction, save_trainer, save_training_specs,
-          save_results, execution_dir, trackings_dir):
+def _json_default(o):
+    # Hack to save numpy number with JSON module
+    if isinstance(o, np.int32) or isinstance(o, np.int64):
+        return int(o)
+    if isinstance(o, np.float32) or isinstance(o, np.float64):
+        return float(o)
+    raise TypeError
+
+
+def read_json(file_path, *args, **kwargs):
+    """
+    Read JSON
+
+    Parameters
+    ----------
+    file_path: str
+        JSON file path
+
+    Returns
+    -------
+    json: dict
+        JSON data.
+    """
+    with open(file_path) as f:
+        return json.load(f, *args, **kwargs)
+
+
+def save_to_json(path, obj, indent=4, *args, **kwargs):
+    """
+    Save data to JSON file.
+
+    Parameters
+    ----------
+    path: str
+        JSON file path.
+    obj: dict or list
+        Object to save
+    indent: int, optional
+        Indentation to save file (pretty print JSON)
+    """
+    with open(path, "w") as outfile:
+        json.dump(obj, outfile, indent=indent, cls=NDArrayEncoder, default=_json_default, *args, **kwargs)
+
+
+def train(training_specs_json, download_dir, force_download, force_extraction, no_save_trainer, no_save_training_specs,
+          no_save_results, execution_dir, trackings_dir):
+
+    from gymnos.trainer import Trainer
+    from gymnos.services.download_manager import DownloadManager
 
     training_config = read_json(training_specs_json, object_pairs_hook=OrderedDict)
 
-    trainer = Trainer.from_spec(training_config)
+    trainer = Trainer.from_dict(training_config)
 
     dl_manager = DownloadManager(download_dir, force_download=force_download,
                                  force_extraction=force_extraction)
@@ -43,70 +100,119 @@ def train(training_specs_json, download_dir, force_download, force_extraction, s
 
     logger = logging.getLogger(__name__)
 
+    logger.info("Execution directory will be located at {}".format(execution_dir))
+    logger.info("Trackings directory will be located at {}".format(trackings_dir))
+
     try:
         training_results = trainer.train(dl_manager, trackings_dir=trackings_dir)
     except Exception as e:
         logger.exception("Exception ocurred: {}".format(e))
         raise
 
-    if save_trainer:
-        trainer.save(os.path.join(execution_dir, "saved_trainer.zip"))
+    if not no_save_trainer:
+        path = os.path.join(execution_dir, "saved_trainer.zip")
+        logger.info("Saving trainer to {}".format(path))
+        trainer.save(path)
 
-    if save_training_specs:
-        save_to_json(os.path.join(execution_dir, "training_specs.json"), training_config)
+    if not no_save_training_specs:
+        path = os.path.join(execution_dir, "training_specs.json")
+        logger.info("Saving training specs to {}".format(path))
+        save_to_json(path, training_config)
 
-    if save_results:
-        save_to_json(os.path.join(execution_dir, "results.json"), training_results)
-
-
-def predict(samples_json, trainer):
-    trainer = Trainer.load(trainer)
-
-    samples = read_json(samples_json)
-
-    predictions = trainer.predict(samples)
-
-    print(predictions)
+    if not no_save_results:
+        path = os.path.join(execution_dir, "results.json")
+        logger.info("Saving results to {}".format(path))
+        save_to_json(path, training_results)
 
 
-def main():
+def predict(saved_trainer, json_file=None, images=None):
+    from gymnos.trainer import Trainer
+    from gymnos.datasets.dataset import ClassLabel
+
+    if images is None:
+        samples = np.array(read_json(json_file))
+    else:
+        # images can be a nested list so we flatten the list
+        samples = np.array([np.array(Image.open(image).convert("RGB")) for image in itertools.chain(*images)])
+
+    trainer = Trainer.load(saved_trainer)
+
+    response = dict(predictions=trainer.predict(samples))
+
+    try:
+        response["probabilities"] = trainer.predict_proba(samples)
+    except NotImplementedError:
+        pass
+
+    labels = trainer.dataset.dataset.info().labels
+    if isinstance(labels, ClassLabel):
+        response["classes"] = dict(
+            names=labels.names,
+            total=labels.num_classes
+        )
+
+    print(json.dumps(response, cls=NDArrayEncoder, indent=4))
+
+
+
+
+def build_parser():
     parser = argparse.ArgumentParser(description="Gymnos tool")
 
     subparsers = parser.add_subparsers(dest="command")
 
-    train_parser = subparsers.add_parser("train")
-    train_parser.add_argument("training_specs_json", help="Training configuration JSON path", type=str)
+    # MARK: Train parsers
 
+    train_parser = subparsers.add_parser("train", help="Train gymnos trainer using a JSON file")
+    train_parser.add_argument("training_specs_json", help=("Training configuration JSON path. " +
+                                                           "This will call Trainer.from_dict() with the JSON file."),
+                              type=str)
     train_parser.add_argument("--download_dir", help="Directory to download datasets", type=str, default="downloads")
     train_parser.add_argument("--force_download", help="Whether or not force download if file already exists",
                               action="store_true", default=False)
     train_parser.add_argument("--force_extraction", help="Whether or not force extraction if file already exists",
                               action="store_true", default=False)
+    train_parser.add_argument("--no-save_trainer", help="Whether or not save trainer after training",
+                              action="store_true", default=False)
+    train_parser.add_argument("--no-save_training_specs", help="Whether or not save JSON training configuration file",
+                              action="store_true", default=False)
+    train_parser.add_argument("--no-save_results", help="Whether or not save results", action="store_true",
+                              default=False)
+    train_parser.add_argument("--execution_dir", help="Execution directory to store training outputs. It accepts the" +
+                                                      " following format arguments: dataset_name, model_name, uuid",
+                              type=str, default="trainings/{dataset_name}/executions/{uuid}")
+    train_parser.add_argument("--trackings_dir", help="Execution directory to store tracking outputs. It accepts the" +
+                                                      " following format arguments: dataset_name, model_name, uuid",
+                              type=str, default="trainings/{dataset_name}/trackings")
 
-    train_parser.add_argument("--save_trainer", help="Whether or not save trainer after training", action="store_true",
-                              default=True)
-    train_parser.add_argument("--save_training_specs", help="Whether or not save JSON training configuration file",
-                              type=str, default=True)
-    train_parser.add_argument("--save_results", help="Whether or not save results", type=str, default=True)
-
-    train_parser.add_argument("--execution_dir", help="Execution directory to store training outputs", type=str,
-                              default="trainings/{dataset_name}/executions/{uuid}")
-    train_parser.add_argument("--trackings_dir", help="Trackings directory", type=str,
-                              default="trainings/{dataset_name}/trackings")
+    # MARK: Predict parsers
 
     predict_parser = subparsers.add_parser("predict")
-    predict_parser.add_argument("samples_json", help="Samples to predict", type=str)
-    predict_parser.add_argument("--trainer", help="Saved trainer file", type=str, required=True)
+    predict_parser.add_argument("saved_trainer", help="Saved trainer file", type=str)
+
+    predict_parser_group = predict_parser.add_mutually_exclusive_group(required=True)
+    predict_parser_group.add_argument("--json", help="JSON file path with list of samples to predict", type=str)
+    predict_parser_group.add_argument("--image", help="Image file path to predict", nargs="*", type=str,
+                                      action="append")
+
+    return parser
+
+
+def main():
+    parser = build_parser()
 
     args = parser.parse_args()
 
     if args.command == "train":
         train(args.training_specs_json, download_dir=args.download_dir, force_download=args.force_download,
-              force_extraction=args.force_extraction, save_trainer=args.save_trainer,
-              save_training_specs=args.save_training_specs, save_results=args.save_results,
+              force_extraction=args.force_extraction, no_save_trainer=args.no_save_trainer,
+              no_save_training_specs=args.no_save_training_specs, no_save_results=args.no_save_results,
               execution_dir=args.execution_dir, trackings_dir=args.trackings_dir)
     elif args.command == "predict":
-        predict(args.samples_json, trainer=args.trainer)
+        predict(args.saved_trainer, args.json, args.image)
+    else:
+        parser.print_help()
 
 
-main()
+if __name__ == "__main__":
+    main()
