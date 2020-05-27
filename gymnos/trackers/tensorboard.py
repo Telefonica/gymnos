5ea82f1aac3c345870e15ee4fc3a3ee760ffe20b
Ruben Salas <ruben.salasalcornada@telefonica.com>
2020-05-28 01:06:10 +0200

    Use tensorboard with pytorch if tf is not installed

--- gymnos/trackers/tensorboard.py
+++ gymnos/trackers/tensorboard.py
@@ -7,8 +7,6 @@
 
 import os
 
-from io import BytesIO
-
 from .tracker import Tracker
 from ..utils.lazy_imports import lazy_imports as lazy
 
@@ -19,46 +17,45 @@ class TensorBoard(Tracker):
     """
 
     def start(self, run_name, logdir):
-        self.writer = lazy.tensorflow.summary.FileWriter(os.path.join(logdir, "tensorboard", run_name))
-
-    def log_tag(self, key, value):
-        pass
-
-    def log_asset(self, name, file_path):
-        pass
-
-    def log_param(self, name, value, step=None):
-        pass
+        self.writer = SummaryWriter(os.path.join(logdir, "tensorboard", run_name))
 
     def log_metric(self, name, value, step=None):
-        summary = lazy.tensorflow.Summary(value=[lazy.tensorflow.Summary.Value(tag=name, simple_value=value)])
-        self.writer.add_summary(summary, step)
-
-    def log_image(self, name, file_path):
-        image = lazy.PIL.Image.open(file_path)
-
-        with BytesIO() as buffer:
-            image.save(buffer, format="JPEG")
-            img_summary = lazy.tensorflow.Summary.Image(encoded_image_string=buffer.getvalue(),
-                                                        width=image.width,
-                                                        height=image.height)
-
-        summary = lazy.tensorflow.Summary(value=[lazy.tensorflow.Summary.Value(tag=name, image=img_summary)])
-        self.writer.add_summary(summary)
-
-    def log_figure(self, name, figure):
-        buffer = BytesIO()
-        figure.savefig(buffer, format='png')
-        buffer.seek(0)
-        img = lazy.PIL.Image.open(buffer.getbuffer())
-
-        img_summary = lazy.tensorflow.Summary.Image(encoded_image_string=buffer.getvalue(),
-                                                    height=img.shape[0],
-                                                    width=img.shape[1])
-
-        summary = lazy.tensorflow.Summary(value=[lazy.tensorflow.Summary.Value(tag=name, image=img_summary)])
-        self.writer.add_summary(summary)
+        self.writer.add_scalar(name, value, step)
 
     def end(self):
         self.writer.flush()
         self.writer.close()
+
+
+class SummaryWriter:
+
+    def __init__(self, logdir):
+        try:
+            from tensorflow.summary import FileWriter as Writer
+            mode = "tf"
+        except ImportError:
+            try:
+                from torch.utils.tensorboard import SummaryWriter as Writer
+                mode = "torch"
+            except ImportError:
+                _ = lazy.tensorflow
+                from tensorflow.summary import FileWriter as Writer
+                mode = "tf"
+
+        self.mode = mode
+        self.writer = Writer(logdir)
+
+    def add_scalar(self, tag, scalar_value, step=None):
+        if self.mode == "tf":
+            summary = lazy.tensorflow.Summary(value=[lazy.tensorflow.Summary.Value(tag=tag, simple_value=scalar_value)])
+            self.writer.add_summary(summary, step)
+        elif self.mode == "torch":
+            self.writer.add_scalar(tag, scalar_value, step)
+        else:
+            raise ValueError("Unknown mode: {}".format(self.mode))
+
+    def close(self):
+        self.writer.close()
+
+    def flush(self):
+        self.writer.flush()
