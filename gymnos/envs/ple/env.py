a7ebe24056265cb61dde6a2915844317d01f0039
Rubén Salas <ruben.salasalcornada@telefonica.com>
2021-08-17 13:20:46 +0200

    Explicit grayscale obs parameter for PLE env

--- gymnos/envs/ple/env.py
+++ gymnos/envs/ple/env.py
@@ -10,9 +10,11 @@ import gym_ple  # noqa: DO NOT REMOVE IMPORT
 from supersuit import frame_stack_v1, color_reduction_v0
 
 
-def PLE(name, use_wrapper: bool = False, frame_stack: int = 0):
+def PLE(name, use_wrapper: bool = False, grayscale_obs=True, frame_stack: int = 0):
     env = gym.make(name + "-v0")
     if use_wrapper:
+        if grayscale_obs:
+            env = color_reduction_v0(env)
         if frame_stack > 0:
-            env = frame_stack_v1(color_reduction_v0(env), frame_stack)
+            env = frame_stack_v1(env, frame_stack)
     return env
--- gymnos/envs/ple/hydra_conf.py
+++ gymnos/envs/ple/hydra_conf.py
@@ -13,5 +13,6 @@ class PLEHydraConf:
     name: str
     use_wrapper: bool = True
     frame_stack: int = 0
+    grayscale_obs: bool = True
 
     _target_: str = field(init=False, repr=False, default="gymnos.envs.ple.env.PLE")
